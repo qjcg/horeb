4c4ccfb01a6edcc47d1ed3dd827df132dd000269
John Gosset <john.gosset@gmail.com>
2015-02-22 21:19:23 -0500

    Fix terminology (plane -> block)

--- main.go
+++ main.go
@@ -15,23 +15,23 @@ func main() {
 	high := flag.Int("h", 0x10ffff, "high unicode codepoint")
 	nchars := flag.Int("n", 300, "number of characters to print")
 	wrapWidth := flag.Int("w", 60, "line width")
-	plane := flag.String("p", "geometric", "unicode plane by name")
-	printMap := flag.Bool("m", false, "print Planes map")
+	block := flag.String("b", "geometric", "unicode block by name")
+	printMap := flag.Bool("m", false, "print Blocks map")
 	flag.Parse()
 
 	if *printMap {
-		for k, v := range Planes {
+		for k, v := range Blocks {
 			fmt.Printf("%-15s: %U, %U\n", k, v.low, v.high)
 		}
 		os.Exit(0)
 	}
 
 	// If valid plane provided as flag, use it
-	if *plane != "" {
-		p := Planes[*plane]
-		if p.high != 0 {
-			low = &p.low
-			high = &p.high
+	if *block != "" {
+		b := Blocks[*block]
+		if b.high != 0 {
+			low = &b.low
+			high = &b.high
 		}
 	}
 
--- planes.go
+++ planes.go
@@ -1,6 +1,6 @@
 package main
 
-type UnicodePlane struct {
+type UnicodeBlock struct {
 	low, high int
 }
 
@@ -10,19 +10,19 @@ See:
 - http:en.wikipedia.org/wiki/Plane_(Unicode)#Basic_Multilingual_Plane
 - http:en.wikipedia.org/wiki/Plane_(Unicode)#Supplementary_Multilingual_Plane
 */
-var Planes = map[string]*UnicodePlane{
-	"hebrew":             &UnicodePlane{0x0590, 0x05ff},
-	"currency":           &UnicodePlane{0x20a0, 0x20cf},
-	"letterlike":         &UnicodePlane{0x2100, 0x214f},
-	"geometric":          &UnicodePlane{0x25a0, 0x25ff},
-	"misc_symbols":       &UnicodePlane{0x2600, 0x26ff},
-	"dingbats":           &UnicodePlane{0x2700, 0x27bf},
-	"aegean_nums":        &UnicodePlane{0x10100, 0x1013f},
-	"ancient_greek_nums": &UnicodePlane{0x10140, 0x1018f},
-	"phaistos_disc":      &UnicodePlane{0x101d0, 0x101ff},
-	"math_alnum":         &UnicodePlane{0x1d400, 0x1d7ff},
-	"emoji":              &UnicodePlane{0x1f300, 0x1f5ff},
-	"mahjong":            &UnicodePlane{0x1f000, 0x1f02f},
-	"dominos":            &UnicodePlane{0x1f030, 0x1f09f},
-	"playing_cards":      &UnicodePlane{0x1f0a0, 0x1f0ff},
+var Blocks = map[string]*UnicodeBlock{
+	"hebrew":             &UnicodeBlock{0x0590, 0x05ff},
+	"currency":           &UnicodeBlock{0x20a0, 0x20cf},
+	"letterlike":         &UnicodeBlock{0x2100, 0x214f},
+	"geometric":          &UnicodeBlock{0x25a0, 0x25ff},
+	"misc_symbols":       &UnicodeBlock{0x2600, 0x26ff},
+	"dingbats":           &UnicodeBlock{0x2700, 0x27bf},
+	"aegean_nums":        &UnicodeBlock{0x10100, 0x1013f},
+	"ancient_greek_nums": &UnicodeBlock{0x10140, 0x1018f},
+	"phaistos_disc":      &UnicodeBlock{0x101d0, 0x101ff},
+	"math_alnum":         &UnicodeBlock{0x1d400, 0x1d7ff},
+	"emoji":              &UnicodeBlock{0x1f300, 0x1f5ff},
+	"mahjong":            &UnicodeBlock{0x1f000, 0x1f02f},
+	"dominos":            &UnicodeBlock{0x1f030, 0x1f09f},
+	"playing_cards":      &UnicodeBlock{0x1f0a0, 0x1f0ff},
 }
