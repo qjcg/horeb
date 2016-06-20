7b677f18184e5deab4faca8c317d0f55ea50922e
John Gosset <john.gosset@gmail.com>
2016-06-20 18:40:49 -0400

    Various updates

--- main.go
+++ main.go
@@ -3,46 +3,62 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"math/rand"
-	"strings"
 	"time"
 )
+
+const (
+	description = "horeb: Speaking in tongues via stdout"
+)
+
+func usage() {
+	fmt.Printf("\n%s\n\n", description)
+	flag.PrintDefaults()
+	fmt.Println()
+}
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
+	flag.Usage = usage
+
+	color := flag.Bool("c", false, "colorize output")
 	dump := flag.Bool("d", false, "print all Blocks")
 	list := flag.Bool("l", false, "list all Block names and codepoint ranges")
-	nchars := flag.Int("n", 80, "number of characters to print")
-	blocksStr := flag.String("b", "geometric", "comma-separated unicode blocks by name")
-	//blockRangeStr := flag.String("r", "", "one (end) or two (start,end) comma-separated unicode blocks by hex value")
+	nchars := flag.Int("n", 30, "number of characters to print")
 	flag.Parse()
 
-	// Convert multi-valued arguments to slices.
-	blocks := strings.Split(*blocksStr, ",")
-	//blockRange := strings.Split(*blockRangeStr, ",")
+	blocks := []string{"geometric"}
+	if flag.NArg() > 0 {
+		blocks = flag.Args()
+	}
 
 	switch {
+	case *color:
+		// TODO: implement colorized output
+		return
 	case *list:
 		printBlocks(false)
 	case *dump:
-		if len(blocks) != 0 {
-			for _, s := range blocks {
-				b := Blocks[s]
-				b.Print()
-			}
-		} else {
-			printBlocks(true)
-		}
+		printBlocks(true)
 	case len(blocks) == 1:
 		Blocks[blocks[0]].PrintRandom(*nchars)
 	case len(blocks) > 1:
-		bm := map[string]*UnicodeBlock{}
+		var bm map[string]*UnicodeBlock
 		for _, b := range blocks {
-			bm[b] = Blocks[b]
+			if val, ok := Blocks[b]; ok {
+				bm[b] = val
+			}
 		}
-		for i := 0; i < *nchars; i++ {
-			fmt.Printf("%c ", RandomBlock(bm).RandomRune())
+		if len(bm) > 0 {
+			for i := 0; i < *nchars; i++ {
+				block, err := RandomBlock(bm)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf("%c ", block.RandomRune())
+			}
 		}
 		fmt.Println()
 	}
