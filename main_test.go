0adc2b43e481cad80f99612825d5b67c6f7a9312
John Gosset <john.gosset@gmail.com>
2016-06-20 18:41:02 -0400

    Add integration tests

---
+++ main_test.go
@@ -0,0 +1,56 @@
+// +build integration
+
+package main
+
+// Integration tests; must be run after successful "go install".
+// go test -v -tags integration
+
+import (
+	"os/exec"
+	"testing"
+)
+
+const cmdName = "horeb"
+
+func TestNoArgs(t *testing.T) {
+	out, err := exec.Command(cmdName).CombinedOutput()
+	if err != nil {
+		t.Fatalf("%s\n%s\n", err, out)
+	}
+}
+
+func TestBadArg(t *testing.T) {
+	out, err := exec.Command(cmdName, "NOT_A_BLOCK").CombinedOutput()
+	if err != nil {
+		t.Fatalf("%s\n%s\n", err, out)
+	}
+}
+
+func TestGoodArg(t *testing.T) {
+	tests := []string{
+		"geometric",
+		"mahjong",
+		"emoji",
+	}
+	for _, block := range tests {
+		out, err := exec.Command(cmdName, block).CombinedOutput()
+		if err != nil {
+			t.Fatalf("%s\n%s\n", err, out)
+		}
+	}
+}
+
+func TestMultiGoodArgs(t *testing.T) {
+	tests := []struct {
+		blocks []string
+	}{
+		{[]string{"geometric", "dominos"}},
+		{[]string{"emoji", "mahjong"}},
+	}
+	for _, test := range tests {
+		out, err := exec.Command(cmdName, test.blocks...).CombinedOutput()
+		if err != nil {
+			t.Fatalf("%s\n%s\n", err, out)
+		}
+	}
+}
