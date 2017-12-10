0224b5389250659a06b6c65d177927b0a47e41ff
John Gosset <john@gossetx.com>
2017-12-10 05:27:17 +0100

    Tweak horebd

--- cmd/horebd/main.go
+++ cmd/horebd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net"
 
@@ -8,24 +10,32 @@ import (
 	pb "github.com/qjcg/horeb/proto"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
 )
 
 // server is used to implement our horeb server.
 type server struct{}
 
 func (s server) GetStream(rr *pb.RuneRequest, stream pb.Horeb_GetStreamServer) error {
+	grpclog.Printf("Got: %#v", rr)
 	for i := 0; i < int(rr.Num); i++ {
 
-		myRandomRune := horeb.Blocks["geometric"].RandomRune()
-		if err := stream.Send(&pb.Rune{string(myRandomRune)}); err != nil {
+		myRandomRune := pb.Rune{R: string(horeb.Blocks["geometric"].RandomRune())}
+		if err := stream.Send(&myRandomRune); err != nil {
 			return err
 		}
+		grpclog.Printf("Sent: %#v", myRandomRune)
 	}
 	return nil
 }
 
 func main() {
-	lis, err := net.Listen("tcp", ":9999")
+	ip := flag.String("i", "127.0.0.1", "ip address to listen on")
+	port := flag.String("p", "9999", "TCP port to listen on")
+	flag.Parse()
+
+	listenString := fmt.Sprintf("%s:%s", *ip, *port)
+	lis, err := net.Listen("tcp", listenString)
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
@@ -33,7 +43,7 @@ func main() {
 	s := grpc.NewServer()
 	pb.RegisterHorebServer(s, &server{})
 
-	log.Println("Horeb gRPC server listening on tcp://0.0.0.0:9999")
+	grpclog.Printf("Horeb gRPC server listening on tcp://%s", listenString)
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
