f72884e2b54fa56ee03777a82d6368a6dd9fdd3b
John Gosset <john@gossetx.com>
2017-12-10 03:17:14 +0100

    Refactor existing code into horeb library & horebcli cmd

--- cmd/horebcli/main.go
+++ cmd/horebcli/main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"math/rand"
 	"time"
+
+	"github.com/qjcg/horeb"
 )
 
 const (
@@ -43,26 +45,26 @@ func main() {
 	if blocks[0] == "all" {
 		// remove "all" value after use
 		blocks = blocks[:0]
-		for k := range Blocks {
+		for k := range horeb.Blocks {
 			blocks = append(blocks, k)
 		}
 	}
 
 	switch {
 	case *list:
-		printBlocks(false)
+		horeb.PrintBlocks(false)
 	case *dump:
-		printBlocks(true)
+		horeb.PrintBlocks(true)
 	case len(blocks) == 1:
-		b, ok := Blocks[blocks[0]]
+		b, ok := horeb.Blocks[blocks[0]]
 		if !ok {
 			log.Fatalf("Unknown block: %s\n", blocks[0])
 		}
 		b.PrintRandom(*nchars, *ofs)
 	case len(blocks) > 1:
-		bm := map[string]UnicodeBlock{}
+		bm := map[string]horeb.UnicodeBlock{}
 		for _, b := range blocks {
-			val, ok := Blocks[b]
+			val, ok := horeb.Blocks[b]
 			if !ok {
 				log.Printf("Unknown block: %s\n", b)
 				continue
@@ -71,7 +73,7 @@ func main() {
 		}
 		if len(bm) > 0 {
 			for i := 0; i < *nchars; i++ {
-				block, err := RandomBlock(bm)
+				block, err := horeb.RandomBlock(bm)
 				if err != nil {
 					log.Fatal(err)
 				}
