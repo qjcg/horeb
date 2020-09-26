c0e729eab58ec790ffbd02f3cae9ba09140cacd3
John Gosset <john@gossetx.com>
2020-09-25 23:32:28 -0400

    feat: Generate interesting patterns

--- cmd/horebd/main.go
+++ cmd/horebd/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net"
 	"os"
 
 	"github.com/qjcg/horeb/pkg/horeb"
 	pb "github.com/qjcg/horeb/proto"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
@@ -15,43 +17,57 @@ import (
 
 var logger = grpclog.NewLoggerV2(os.Stderr, os.Stderr, os.Stderr)
 
-func main() {
-	ip := flag.String("i", "0.0.0.0", "ip address to listen on")
-	port := flag.Int("p", 9999, "TCP port to listen on")
-	version := flag.Bool("v", false, "print version")
-	flag.Parse()
+// Conf represents this app's configuration.
+type Conf struct {
+	logger *logrus.Logger
+	viper  *viper.Viper
+}
 
-	if *version {
+func main() {
+	pflag.StringP("ip", "i", "0.0.0.0", "ip address to listen on")
+	pflag.UintP("port", "p", 9999, "TCP port to listen on")
+	pflag.BoolP("version", "v", false, "print version")
+	pflag.Parse()
+
+	viper.SetEnvPrefix("HOREBD")
+	viper.AutomaticEnv()
+
+	conf := Conf{
+		logger: logrus.New(),
+		viper:  viper.GetViper(),
+	}
+
+	if viper.GetBool("version") {
 		fmt.Println(horeb.Version)
 		return
 	}
 
 	var lis net.Listener
-	listenFlags := fmt.Sprintf("%s:%v", *ip, *port)
+	listenFlags := fmt.Sprintf("%s:%v", viper.GetString("ip"), viper.GetString("port"))
 	lis, err := net.Listen("tcp", listenFlags)
 	if err != nil {
-		logger.Fatalf("Failed to listen: %v", err)
+		conf.logger.Fatalf("Failed to listen: %v", err)
 	}
-	logger.Infof("Using flag listener: %#v\n", lis)
+	conf.logger.Infof("Using flag listener: %#v\n", lis)
 
 	s := grpc.NewServer()
 	pb.RegisterHorebServer(s, &server{})
 
-	logger.Infof("Horeb gRPC server listening on %#v", lis)
+	conf.logger.Infof("Horeb gRPC server listening on %#v", lis)
 
 	if err := s.Serve(lis); err != nil {
-		logger.Fatalf("failed to serve: %#v", err)
+		conf.logger.Fatalf("failed to serve: %#v", err)
 	}
 }
 
 // server is used to implement our horeb server.
 type server struct{}
 
-func (s server) GetStream(rr *pb.RuneRequest, stream pb.Horeb_GetStreamServer) error {
-	logger.Infof("RECEIVED: %#v", rr)
+func (s server) GetStream(conf *Conf, rr *pb.RuneRequest, stream pb.Horeb_GetStreamServer) error {
+	conf.logger.Info("RECEIVED: %#v", rr)
 	block, ok := horeb.Blocks[rr.Block]
 	if !ok {
-		logger.Errorf("Invalid block: %s\n", rr.Block)
+		conf.logger.Errorf("Invalid block: %s\n", rr.Block)
 		return fmt.Errorf("Invalid block: %s", rr.Block)
 	}
 	for i := 0; i < int(rr.Num); i++ {
@@ -60,6 +76,6 @@ func (s server) GetStream(rr *pb.RuneRequest, stream pb.Horeb_GetStreamServer) e
 			return err
 		}
 	}
-	logger.Infof("SENT: %d %s\n", rr.Num, rr.Block)
+	conf.logger.Infof("SENT: %d %s\n", rr.Num, rr.Block)
 	return nil
 }
