39fcd6e67adc010365813e99fa2c40df822e6016
John Gosset <john@gossetx.com>
2022-11-27 06:29:36 -0500

    test: Switch to `testscript` for integration testing

--- cmd/horeb/main_test.go
+++ cmd/horeb/main_test.go
@@ -2,71 +2,21 @@
 
 package main
 
-// Integration tests; must be run after successful "go install".
-// go test -v -tags integration
-
 import (
 	"os"
-	"os/exec"
-	"path"
 	"testing"
+
+	"github.com/rogpeppe/go-internal/testscript"
 )
 
-const cmdName = "horeb"
+func TestMain(m *testing.M) {
+	os.Exit(testscript.RunMain(m, map[string]func() int{
+		"horebMain": Main,
+	}))
+}
 
-func TestBuildAndRunBinary(t *testing.T) {
-	dir, err := os.MkdirTemp("", cmdName)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(dir)
-
-	binPath := path.Join(dir, cmdName)
-	err = exec.Command("go", "build", "-o", binPath).Run()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	t.Run("NoArgs", func(t *testing.T) {
-		err := exec.Command(binPath).Run()
-		if err != nil {
-			t.Fatal(err)
-		}
-	})
-
-	t.Run("BadArg", func(t *testing.T) {
-		err := exec.Command(binPath, "NOT_A_BLOCK").Run()
-		if _, ok := err.(*exec.ExitError); !ok {
-			t.Fatal(err)
-		}
-	})
-
-	t.Run("GoodArg", func(t *testing.T) {
-		tests := []string{
-			"geometric",
-			"mahjong",
-			"emoji",
-		}
-		for _, block := range tests {
-			out, err := exec.Command(binPath, block).CombinedOutput()
-			if err != nil {
-				t.Fatalf("%s\n%s\n", err, out)
-			}
-		}
-	})
-
-	t.Run("MultiGoodArgs", func(t *testing.T) {
-		tests := []struct {
-			blocks []string
-		}{
-			{[]string{"geometric", "dominos"}},
-			{[]string{"emoji", "mahjong"}},
-		}
-		for _, test := range tests {
-			out, err := exec.Command(binPath, test.blocks...).CombinedOutput()
-			if err != nil {
-				t.Fatalf("%s\n%s\n", err, out)
-			}
-		}
+func TestBin(t *testing.T) {
+	testscript.Run(t, testscript.Params{
+		Dir: "testdata",
 	})
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,9 @@ require golang.org/x/exp v0.0.0-20221126150942-6ab00d035af9
 
 require github.com/samber/lo v1.35.0
 
-require github.com/samber/mo v1.5.1
+require (
+	github.com/rogpeppe/go-internal v1.9.0
+	github.com/samber/mo v1.5.1
+)
+
+require github.com/pkg/diff v0.0.0-20210226163009-20ebb0f2a09e // indirect
